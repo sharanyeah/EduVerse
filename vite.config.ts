91bf6b7926d4cfabdbd7e7968384d7af46c32cce
sharanyeah <kallemsharanyareddy21@gmail.com>
2026-01-16 15:48:49 +0530

    Change minification tool from esbuild to terser

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: 'dist',
       sourcemap: false,
-      minify: 'esbuild',
+      minify: 'terser',
       rollupOptions: {
         output: {
           manualChunks: {
